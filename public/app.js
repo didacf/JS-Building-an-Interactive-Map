efc311526d5fbf3e862fe09958c508f582b3a02a
Didac Fernandez <didac.fernandez@insiemecloud.net>
2022-07-09 15:44:52 +0200

    First Setup

--- public/app.js
+++ public/app.js
@@ -1,9 +1,13 @@
-// create map
-
-
-// add openstreetmap tiles
-
-
+// Create map:                                                       
+const myMap = L.map('map', {
+    center: [48.868672, 2.342130],
+    zoom: 12,
+});
+// Add OpenStreetMap tiles:
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    minZoom: '15',
+}).addTo(myMap)
 // create and main add geolocation marker
 
 
