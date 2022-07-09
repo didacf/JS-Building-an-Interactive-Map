a56601a0b71e4991cf7fad724bf6f16185862b10
Didac Fernandez <didac.fernandez@insiemecloud.net>
2022-07-09 16:25:22 +0200

    First Setup

--- public/app.js
+++ public/app.js
@@ -8,9 +8,10 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     minZoom: '15',
 }).addTo(myMap)
-// create and main add geolocation marker
 
-
+// Create and add a geolocation marker:
+const marker = L.marker([48.87007, 2.346453])
+marker.addTo(myMap).bindPopup('<p1><b>The Hoxton, Paris</b></p1>').openPopup()
 // draw the 2nd arrondissement
 
 
